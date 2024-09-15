d4096e4b0e7d54581089a8f4a16dd0e1e7948412
faraz41k <sarfarazhassan907@gmail.com>
2024-09-15 16:56:07 +0500

    update target link

--- nextmagpie-main/components/CompReview.jsx
+++ nextmagpie-main/components/CompReview.jsx
@@ -13,21 +13,21 @@ const CompReview = () => {
         </div>
 
         <div className='p-2'> 
-            <Link href='/'>
+            <Link href='/' target='_blank'>
             <Image src='/trustpilot.svg' height={100} width={100} alt='logo-imgs'/>
             </Link>
             
         </div>
 
         <div className='p-2'>
-            <Link href='/'>
+            <Link href='/' target='_blank'>
             <Image src='/tripadvisor.svg' height={100} width={100} alt='logo-imgs'/>
             </Link>
             
         </div>
 
         <div className='p-2'>
-            <Link href='/https://www.instagram.com/magpietourspakistan?igsh=MWc0bzcxNWhqOGx3Nw%3D%3D&utm_source=qr'>
+            <Link href='/https://www.instagram.com/magpietourspakistan?igsh=MWc0bzcxNWhqOGx3Nw%3D%3D&utm_source=qr' target='_blank'>
             <Image src='/instagram-2.svg' height={100} width={100} alt=''/>
             </Link>
             
