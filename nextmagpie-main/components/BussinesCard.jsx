7378435f47d5b858b6ed31e492bb69644738b49c
faraz41k <sarfarazhassan907@gmail.com>
2024-09-21 18:49:25 +0500

    card text

--- nextmagpie-main/components/BussinesCard.jsx
+++ nextmagpie-main/components/BussinesCard.jsx
@@ -23,8 +23,8 @@ const BussinesCard = ({
       className="[text-decoration:none] flex-1 rounded-lg h-[241px] hover:opacity-75 flex flex-row items-start justify-start relative bg-[url('/card-1@3x.png')] bg-cover bg-no-repeat bg-[top] text-left text-5xl text-gray-white font-body-regular-600 md:flex-[unset] md:self-stretch"
       style={card11Style}
     >
-      <div className="my-0 mx-[!important] absolute top-[16px]  left-[16px] flex flex-col items-start justify-start gap-[11px] z-[0]">
-        <div className="relative leading-[32px] font-semibold backdrop-blur-sm bg-whitesmoke-200/20 p-2">{cityName}</div>
+      <div className="my-0 mx-[!important] absolute top-[18px]  left-[16px] flex flex-col items-start justify-start gap-[11px] z-[0]">
+        <div className="relative leading-[32px] font-semibold text-[14px] backdrop-blur-sm bg-whitesmoke-200/20 p-2">{cityName}</div>
         <div className="relative text-base leading-[24px] text-center ">
           
         </div>
