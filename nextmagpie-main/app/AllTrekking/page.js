2dbf2fb0532eaca4654efc27e90a61abf117b95c
faraz41k <sarfarazhassan907@gmail.com>
2024-10-11 16:19:06 +0500

    updt

--- nextmagpie-main/app/AllTrekking/page.js
+++ nextmagpie-main/app/AllTrekking/page.js
@@ -16,7 +16,7 @@ const CustomTours = async ({params})=> {
     <div className=' self-stretch  flex flex-col mb-6'>
       {/* hero */}
       <div className=" ">
-      <div className="hero min-h-[550px] w-full bg-cover bg-center relative" style={{backgroundImage: 'url(/trek.jpg)'}}>
+      <div className="hero min-h-[550px] w-full bg-cover bg-center relative" style={{backgroundImage: 'url(/trekkBanner.jpg)'}}>
         <div className="hero-overlay  bg-opacity-60 py-5"></div>
           <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md ">
