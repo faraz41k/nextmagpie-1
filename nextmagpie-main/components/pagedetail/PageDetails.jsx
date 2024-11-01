8f741ed142e6629bf8ab08fdf12e82554f6e3722
faraz41k <sarfarazhassan907@gmail.com>
2024-11-01 21:53:08 +0500

    lang B2B

--- nextmagpie-main/components/pagedetail/PageDetails.jsx
+++ nextmagpie-main/components/pagedetail/PageDetails.jsx
@@ -47,6 +47,12 @@ const PageDetail = ({
       url1,
       url2,
       url3,
+      langO,
+      langIt,
+      langMap,
+      langInc,
+      langNinc,
+      langNote
     }) => {
     const [visible,setVisible] = useState(false)
     const myRef = useRef()
@@ -93,29 +99,29 @@ const PageDetail = ({
 
         <div  className='  flex flex-col items-start justify-center pt-8 lg:xlg:pt-28 gap-[100px] pb-8' >
             <div className='px-[60px] max-w-6xl'>
-                <h1  className='text-5xl font-semibold pb-3 font-poppins' id='Overview'>Overview</h1>
+                <h1  className='text-5xl font-semibold pb-3 font-poppins' id='Overview'>{langO ? 'Riepilogo' : 'Overview'}</h1>
                 <p className='font-poppins'>{overview}</p>
             </div>
 
             
             <div className='px-[60px] flex flex-col flex-wrap max-w-6xl' id='Itinery'>
-                <h1 className='text-[28px] font-semibold pb-3 font-poppins'>Itinerary At Glance</h1>
-                <div className='list-none'>
+                <h1 className='text-[28px] font-semibold pb-3 font-poppins'>{langIt ? 'Itinerario giornaliero' : 'Itinerary At Glance'}</h1>
+                <div className='list-none' >
                     {Itenary.map((item,ind)=>(
                         <>
                         <li  key={ind} className='font-poppins text-[26px] md:text-[20px] sm:text-[18px]'> {item}</li>
                         <br/>
                         </>
                     ))}
-                    
-                    
                 </div>
+                    
+                    
                 
             </div>
             
 
             <div ref={myRef}  className='px-[60px] max-w-6xl' id='Services'>
-                <h1 className='text-5xl font-semibold pb-3 font-poppins'>Services Included</h1>
+                <h1 className='text-5xl font-semibold pb-3 font-poppins'>{langInc ? 'Servizi inclusi': 'Services Included'}</h1>
                 <div className='list-none'>
                     {services.map((item,index)=>(
                         <p key={index} className='font-poppins'><GoDotFill/> {item}</p>
@@ -127,7 +133,7 @@ const PageDetail = ({
             </div>
 
             <div   className='px-[60px] max-w-6xl' id='Services'>
-                <h1 className='text-5xl font-semibold pb-3 font-poppins'>Services Not Included</h1>
+                <h1 className='text-5xl font-semibold pb-3 font-poppins'>{langNinc ? 'Servizi non inclusi' : 'Services Not Included'}</h1>
                 <div className='list-none'>
                 {notIncluded.map((item,index)=>(
                         <p key={index} className='font-poppins'><GoDotFill/> {item}</p>
@@ -138,7 +144,7 @@ const PageDetail = ({
             
 
             <div  className='px-[60px] max-w-6xl' id='Notes'>
-                <h1 className='text-5xl font-semibold pb-3 font-poppins'>Notes For the Trip</h1>
+                <h1 className='text-5xl font-semibold pb-3 font-poppins'>{langNote ? 'Note per il viaggio' : 'Notes For the Trip'}</h1>
                 <div className='list-none flex flex-col gap-2'>
                 {notes.map((item,index)=>(
                         <p key={index} className='font-poppins'><GoDotFill/> {item}</p>
@@ -149,7 +155,7 @@ const PageDetail = ({
             
             <div className='flex flex-col  justify-start px-[60px]  w-full max-w-6xl  mb-4'>
                 <div className='w-full'>
-                <h3 className='text-5xl font-semibold pb-3 font-poppins'>Tour Map</h3>
+                <h3 className='text-5xl font-semibold pb-3 font-poppins'>{langMap ? 'Mappa del tour' : 'Tour Map'}</h3>
                     <img src={urlFor(map).url()}  className='object-cover w-full '  />
                 </div>
             </div>
