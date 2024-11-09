17105db508330f409ef675e9b412cbeefaa6a7a7
faraz41k <sarfarazhassan907@gmail.com>
2024-11-09 22:12:53 +0500

    updt

--- nextmagpie-main/components/ItalianFestivals.js
+++ nextmagpie-main/components/ItalianFestivals.js
@@ -4,13 +4,11 @@
 const overviewData = [
     {
     id: 'overview1',
-    overview1 : " Il popolo Kalash, conosciuto anche come Kalasha, risiede nel distretto di Chitral, nella provincia di Khyber, nelle tre valli di Rumbur, Brumbret e Birir. Questa comunità minoritaria conta solamente 4000 abitanti. Gli studiosi hanno analizzato le pratiche religiose dei Kalash come una fusione dell'antico induismo e delle tradizioni pre-islamiche. Durante l'anno, si tengono diversi festival Kalash, tra cui tre particolarmente popolari tra i visitatori:"
     },
 
 
     {
         id:'overview2',
-        overview2: " Questo itinerario vi condurrà da Islamabad a Chitral, attraverso un suggestivo viaggio panoramico nella valle dello Swat. Vi immergerete in due giorni di festeggiamenti nella valle dei Kalash, vivendo esperienze indimenticabili della loro cultura. Seguirà un incantevole e avventuroso viaggio in auto verso Mastuj e il lago Khalti, dove potrete ammirare fiumi scintillanti e gustare trote di montagna fresche. La strada proseguirà verso la mozzafiata valle di Hunza, dove avrete l'opportunità di esplorare i storici forti di Baltit e Altit, ammirare le acque color smeraldo del lago Attabad e i maestosi coni di Passu. Godetevi una giornata rilassante dedicata allo shopping di souvenir nei vicoli di Karimabad, prima di concludere con una cena speciale a 2700 metri di altitudine presso il punto panoramico di Duikar. Il viaggio terminerà a Islamabad, dove potrete esplorare la città, gustare deliziosi piatti e conservare i ricordi dei vibranti festival Kalash e della magnifica bellezza del Pakistan settentrionale."
     }
 ]
 
