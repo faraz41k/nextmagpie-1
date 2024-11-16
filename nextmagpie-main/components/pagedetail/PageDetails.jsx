4a9e330fadf515924da4ba174b58270193d2ac5b
faraz41k <sarfarazhassan907@gmail.com>
2024-11-16 20:58:16 +0500

    itenarary updt

--- nextmagpie-main/components/pagedetail/PageDetails.jsx
+++ nextmagpie-main/components/pagedetail/PageDetails.jsx
@@ -109,7 +109,7 @@ const PageDetail = ({
                 <div className='list-none' >
                     {Itenary.map((item,ind)=>(
                         <>
-                        <li  key={ind} className='font-poppins text-[26px] md:text-[20px] sm:text-[18px]'><span className='font-semibold text-[26px] '>{langIt ? item.substr(0,10) : item.substr(0,7) }</span> {item}</li>
+                        <li  key={ind} className='font-poppins text-[26px] md:text-[20px] sm:text-[18px]'><span className='font-semibold text-[26px] '>{langIt ? item.substr(0,10) : item.substr(0,7) }</span> {langIt ? item.substr(10) : item.substr(7)}</li>
                         <br/>
                         </>
                     ))}
